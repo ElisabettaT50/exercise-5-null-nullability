ebbaa0030246af52bbdd247502c3dbff1f62602a
ET <tacconellielisabetta50@gmail.com>
2023-12-06 19:25:15 +0100

    exercise 5 nullability

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.nio.channels.NonReadableChannelException;
+
+import static java.lang.Integer.parseInt;
+
+/*
+Scrivere una funzione che accetti in input un numeratore e un denominatore
+Controlli che il numeratore e il denominatore non siano null
+Usare la funzione appena scritta in due blocchi di codice dove si cattura
+l'eventuale eccezione.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Integer nominator = 4;
+        Integer denominator = null;
+        checkNullability(nominator, denominator);
+    }
+
+    private static void checkNullability(Integer numerator, Integer denominator) {
+        Integer result = 0;
+        try {
+            result = numerator / denominator;
+            System.out.println(result);
+        } catch (NullPointerException e) {
+            System.out.println("Type of exception: " + e);
+        } finally {
+            System.out.println("Code finally executed");
+        }
+    }
+}
